e62d536bb207be928a6149850a7563c1f0106779
João Graça <jonymiguelgraca@gmail.com>
2021-03-29 19:04:43 +0100

    Initial Commit

---
+++ TopNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopNews
+//
+//  Created by João Graça on 29/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
