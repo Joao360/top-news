c9d6a8d6600739c125c86dfb3c1177768902fc9b
João Graça <jonymiguelgraca@gmail.com>
2021-03-31 12:25:45 +0100

    Implement table view with categories

---
+++ TopNews/CategoryTableViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  CategoryTableViewCell.swift
+//  TopNews
+//
+//  Created by João Graça on 30/03/2021.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+    
+    let cellView: UIView = {
+       let view = UIView()
+        view.backgroundColor = UIColor.red
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Some Text"
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        addSubview(cellView)
+        cellView.addSubview(label)
+        self.selectionStyle = .none
+        
+        NSLayoutConstraint.activate([
+            cellView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+            cellView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10),
+            cellView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10),
+            cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        label.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        label.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        label.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        label.leftAnchor.constraint(equalTo: cellView.leftAnchor, constant: 20).isActive = true
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
--- TopNews/ViewController.swift
+++ TopNews/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let categories = ["business", "entertainment",  "general", "health", "science", "sports", "technology"]
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = UIColor.white
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupTableView()
     }
 
-
+    func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+        ])
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(CategoryTableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! CategoryTableViewCell
+        cell.backgroundColor = UIColor.white
+        cell.label.text = categories[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
 
